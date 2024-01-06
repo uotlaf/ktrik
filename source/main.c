7eb5f5966a0722a9fe9c62442fe40655071ec4c0
uotlaf <mail@uotlaf.com>
2024-01-05 22:27:09 -0300

    Fix default code

--- source/main.c
+++ source/main.c
@@ -25,9 +25,6 @@ int main(int argc, char* argv[])
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
 			break; // break in order to return to hbmenu
-        if (kDown & KEY_UP) {
-            consoleSelect()
-        }
 	}
 
 	gfxExit();
